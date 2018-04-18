7c5be632e9603b1c4e430001fd943ad1e064bdb2
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-18 13:38:16 -0500

    functional res req

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 // Require express - gives us back a function
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000; // Used to direct traffic
+const express = require('express'); // Import express
+const app = express(); // assign express as a function to a variable
+const PORT = 5000; // Used to direct traffic
 
 allQuotes = require('./all-quotes.js');
 
-app.use(express.static('/server/public'));
-
-app.get('/', function (req, res) {
-    res.send('Happy Wednesday');
+app.get('/', (req, res) => { // request/response function
+    console.log('yep it worked');
+    res.send('Happy Wednesday'); // appends a message to the DOM
 });
 
-app.listen(port, function () {
-    console.log('listening on port', port);
-});
-
-app.get('/all-quotes', function (req, res) {
+app.get('/all-quotes', (req, res) => {
     res.send(allQuotes);
+});
+
+app.listen(PORT, () => { // function sets up listening on PORT
+    console.log(`listening on port ${PORT}`);
+});
+
+app.get('/random-quote', (req, res) => {
+    const randomNumber = Math.floor(Math.random() * (allQuotes.length));
+    console.log(randomNumber);
+    res.send(allQuotes[randomNumber]);
 });
