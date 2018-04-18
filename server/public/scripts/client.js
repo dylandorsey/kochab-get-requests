f29c45aa00e1f02a3e48ca48254b7e33abb2ac5d
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-18 16:16:51 -0500

    all assigned functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,25 +4,38 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery is loaded');
+    generateQuote();
+    appendAllQuotes();
+    $('#quoteBtn').on('click', generateQuote);
+    // appendAllQuotes();
+}
+
+function appendAllQuotes() {
     $.ajax({
         type: 'GET',
         url: '/all-quotes'
     })
-    .then(function(response) {
-        console.log('promise ring');
-        console.log(response);
-    });
-    $('#quoteBtn').on('click', generateQuote);
+        .then(function (response) {
+            $('#allQuotes').append(`<h3>Here are all of the quotes:</h3>`);
+            for (i = 0; i < response.length; i++) {
+                $('#allQuotes').append(`
+                <p>${response[i].quote}<p>
+                `);
+            }
+
+        });
 }
 
-function generateQuote(){
+function generateQuote() {
     $.ajax({
         type: 'GET',
         url: '/random-quote'
     })
-    .then(function(response) {
-        // console.log('promise ring');
-        $('#quoteOutput').text(response.quote);
-    });
+        .then(function (response) {
+            // console.log('promise ring');
+            $('#quoteOutput').empty();
+            $('#quoteOutput').append(`<h3>Here's a random quote:</h3>`);
+            $('#quoteOutput').append(`<p>${response.quote}`);
+        });
 
 }
